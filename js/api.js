3f1ce1ccd605f00d5e31044269b224b924ff5a7a
yasmimcosm <yasmimcosm@gmail.com>
2026-02-09 23:59:59 -0300

    Mudanças

---
+++ js/api.js
@@ -0,0 +1,14 @@
+    const api = {
+        async buscarPensamentos() {
+            try {
+                const response = await fetch('http://localhost:3000/pensamentos');
+                return response.json();
+            }
+            catch (error) {
+                alert('Erro ao buscar pensamentos');
+                throw error;
+            }
+        }
+    }
+
+    export default api;
